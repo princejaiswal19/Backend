0305b8a72e7ccd6ecdbbdb00956bb10bdc2d8103
prince <masterprincescs430@gmail.com>
2026-08-30 13:02:14 +0530

    polygon testing for the post is successful

--- src/index.js
+++ src/index.js
@@ -7,10 +7,19 @@ import {DB_NAME} from "./constants.js"
 
 
 import connectDB from "./db/index.js";
+import {app} from "./app.js";
 
-dotenv.config({ path: "./env" });
+dotenv.config();
 
-connectDB();
+connectDB()
+.then(()=>{
+  app.listen(process.env.PORT || 8000,()=>{
+    console.log(`server is running at port:${process.env.PORT || 8000}`);
+  })
+})
+.catch((err)=>{
+console.log("MongoDB connection error", err);
+})
 
 
 
--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -1,13 +1,13 @@
 class ApiError extends Error{
     constructor(statusCode, message="Something went wrong",
-        error =[],
+        errors =[],
         stack = ""
     ){
         super(message)
         this.statusCode=statusCode
-        this.data =null
-        this.message=message
-        this.sucess = false
+        this.data = null
+        this.message = message
+        this.success = false
         this.errors = errors
 
         if(stack){
